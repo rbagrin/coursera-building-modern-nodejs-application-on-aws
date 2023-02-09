458c36001a6286760280d38e5023e6e117d45e23
Radu Bagrin <bagrin.radu@gmail.com>
2023-02-09 22:05:19 +0000

    Exercise 3

--- listDragons.js
+++ listDragons.js
@@ -12,88 +12,86 @@
 var AWS = require("aws-sdk");
 
 const s3 = new AWS.S3({
-    region: 'eu-west-2'
+  region: "eu-west-2",
 });
 
 const ssm = new AWS.SSM({
-    region: 'eu-west-2'
+  region: "eu-west-2",
 });
 
 async function readDragons() {
-   var fileName = await getFileName();
-   var bucketName = await getBucketName();
-   return readDragonsFromS3(bucketName, fileName);
-   
+  var fileName = await getFileName();
+  var bucketName = await getBucketName();
+  return readDragonsFromS3(bucketName, fileName);
 }
 
 async function getFileName() {
- var fileNameParams = {
-    Name: 'dragon_data_file_name', 
-    WithDecryption: false
+  var fileNameParams = {
+    Name: "dragon_data_file_name",
+    WithDecryption: false,
   };
   var promise = await ssm.getParameter(fileNameParams).promise();
-  console.log(promise)
+  console.log(promise);
   return promise.Parameter.Value;
-  
 }
 
 async function getBucketName() {
   var bucketNameParams = {
-    Name: 'dragon_data_bucket_name', 
-    WithDecryption: false
+    Name: "dragon_data_bucket_name",
+    WithDecryption: false,
   };
-  
+
   var promise = await ssm.getParameter(bucketNameParams).promise();
   return promise.Parameter.Value;
-
 }
 
 function readDragonsFromS3(bucketName, fileName) {
- 
-  s3.selectObjectContent({
-     Bucket: bucketName,
-     Expression: "select * from S3Object[*][*] s",
-     ExpressionType: 'SQL',
-     Key: fileName,
-     InputSerialization: {
-       JSON: {
-         Type: 'DOCUMENT',
-       }
-     },
-     OutputSerialization: {
-       JSON: {
-         RecordDelimiter: ','
-       }
-     }
-   }, function(err, data) {
-     if (err) {
-         console.log(err);
-     } else {
-       handleData(data);
-     }
-   }
+  s3.selectObjectContent(
+    {
+      Bucket: bucketName,
+      Expression: "select * from S3Object[*][*] s",
+      ExpressionType: "SQL",
+      Key: fileName,
+      InputSerialization: {
+        JSON: {
+          Type: "DOCUMENT",
+        },
+      },
+      OutputSerialization: {
+        JSON: {
+          RecordDelimiter: ",",
+        },
+      },
+    },
+    function (err, data) {
+      if (err) {
+        console.log(err);
+      } else {
+        handleData(data);
+      }
+    }
   );
 }
 
 function handleData(data) {
   var event = data.Payload;
   const resultStream = [];
-  data.Payload.on('data', (event) => {
-		if (event.Records) {
+  data.Payload.on("data", (event) => {
+    if (event.Records) {
       resultStream.push(event.Records.Payload);
-			console.log(event.Records.Payload.toString());
-		}
-	});
-  
-  event.on("end", function () {
-      let recordsString = Buffer.concat(resultStream).toString('utf8');
-      // remove any trailing commas
-      recordsString = recordsString.replace(/\,$/, '');
-      // parse as an array
-      recordsString = `[${recordsString}]`;
-      let records = JSON.parse(recordsString);
+      console.log(event.Records.Payload.toString());
+    }
+  });
 
-      console.log(JSON.stringify(records));
+  event.on("end", function () {
+    let recordsString = Buffer.concat(resultStream).toString("utf8");
+    // remove any trailing commas
+    recordsString = recordsString.replace(/\,$/, "");
+    // parse as an array
+    recordsString = `[${recordsString}]`;
+    let records = JSON.parse(recordsString);
+
+    console.log(JSON.stringify(records));
   });
 }
 
